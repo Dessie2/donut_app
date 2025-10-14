94002576bad12a3a3d4a62b5a5884b891392b9e0
Dessie2 <varguezdessire85@gmail.com>
2025-10-14 16:20:38 -0600

    TabBar added

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:donut_app/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -8,28 +9,62 @@ class Homepage extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<Homepage> {
+  List<Widget> myTabs = [
+    //donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png', label: 'Donut'),
+    //burguer tab
+    const MyTab(iconPath: 'lib/icons/burger.png', label: 'Burguer'),
+    //smoothie tab
+    const MyTab(iconPath: 'lib/icons/smoothie.png', label: 'Smoothie'),
+    //pancake tab
+    const MyTab(iconPath: 'lib/icons/pancakes.png', label: 'Pancake'),
+    //pizza tab
+    const MyTab(iconPath: 'lib/icons/pizza.png', label: 'Pizza'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        ///icono
-        leading: Icon(Icons.menu, color:Colors.grey[800]),
-        //icono de la derecha
-        actions:[
-          Padding(
-            padding: const EdgeInsets.only(right:24.0),
-            child: Icon(Icons.person),
-          )
-        ]
-      ),
-      body: const Column(
-        children: [
-          //Texto principal
-          //Pestaña o tabbar
-          //Contenido
-          //Carito
-        ]
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+
+          ///icono
+          leading: Icon(Icons.menu, color: Colors.grey[800]),
+          //icono de la derecha
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            //Texto principal
+            const Padding(
+              padding: EdgeInsets.only(left: 24.0),
+              child: Row(
+                children: [
+                  Text('I want to  ', style: TextStyle(fontSize: 24)),
+                  Text(
+                    'Eat',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //Pestaña o tabbar
+            TabBar(tabs: myTabs),
+            //Contenido
+            //Carito
+          ],
+        ),
       ),
     );
   }
